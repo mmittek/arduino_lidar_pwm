b1c79e4bf60d0f7a2a7a843c9713a5c66d62dc9c
Mateusz Mittek <mmittek@gmail.com>
2018-09-16 17:04:14 -0500

    Changed the prescaler to overcome issues with the overflow

--- lidar_pwm/lidar_pwm.ino
+++ lidar_pwm/lidar_pwm.ino
@@ -1,14 +1,16 @@
 volatile uint32_t timer_value = 0;
-volatile uint16_t ovf_n = 0;
+volatile uint32_t ovf_n = 0;
 volatile uint8_t led_state = 0;
-float dt = 0;
+double dt = 0;
 int counter = 0;
 uint32_t sample_id = 0;
+volatile uint32_t overflows_n = 0;
 
 #define CONV_SEC_TO_CM  1e5f
 #define CONV_SEC_TO_M   1e3f
 
 #define CONV_SEC_TO_FT  3280.83989501f
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,29 +28,29 @@ Serial.begin(115200);
 
   // Timer
   TCCR1A &= ~(0x03); // normal mode
-  TCCR1B = 0x05;  // 1024 prescaler
 }
 
 inline void start_timer() {
-  TCCR1B = 0;
-    TCNT1L = 0;
-  TCNT1H = 0;
-  ovf_n = 0;
-  TIFR1 &= ~(0x01); // clear overflow flag
-  TIMSK1 |= 0x01; // enable overflow interrupt
-  TCCR1B = 0x02;  // clock /8
+  TCCR1B = 0;       // stop the timer
+  TCNT1L = 0;       //reset the value L 
+  TCNT1H = 0;       // reset the value H
+  ovf_n = 0;        // set the number of overflows to 0
+  EIFR &= ~(0x01);  // clear the interrupt flag on the PD2
+  TIMSK1 |= 0x01;   // enable overflow interrupt
+  TCCR1B = 0x03;    // start the timner
 }
 
 inline void stop_tmimer() {
+  TCCR1B = 0;         // literally stop it
   TIMSK1 &= ~(0x01);  // disable timer 1 interrupt
-  TCCR1B = 0;
-    TCNT1L = 0;
-  TCNT1H = 0;
+  TCNT1L = 0;         // reset the LOW byte of the value
+  TCNT1H = 0;         // reset the high byte of the value
 
 }
 
-ISR(TIM1_OVF_vect) {      // interrupt overflow routine
+ISR(TIMER1_OVF_vect) {      // interrupt overflow routine
   ovf_n++;
+  overflows_n++;
 }
 
 ISR (INT0_vect)
@@ -61,20 +63,21 @@ ISR (INT0_vect)
       timer_value = 0;
       timer_value |= TCNT1L;
       timer_value |= (TCNT1H<<8);
-      timer_value |= (((uint32_t)ovf_n)<<16);
+//      timer_value |= (((uint32_t)ovf_n)<<16);
       stop_tmimer();
     }
-     EIFR &= ~(0x01);  // clear the interrupt flag
+//     EIFR &= ~(0x01);  // clear the interrupt flag
 }
 
 void loop() {
   sample_id++;
-  dt = timer_value*5e-7;
+  dt = timer_value * 0.000004f;
   
   
   // put your main code here, to run repeatedly:
   Serial.print(sample_id);
-  Serial.print(", ");
+  Serial.print(", timer value: ");
+  Serial.print(timer_value);
   Serial.print(", dt: ");
   Serial.print(dt, 4);
   Serial.print(" sec ");
@@ -85,9 +88,11 @@ void loop() {
   Serial.print(", dist [ft]: ");
   Serial.print( dt*CONV_SEC_TO_FT, 2 );
 
+  Serial.print(", overflows: ");
+  Serial.print( overflows_n );
   Serial.println("");
   
-
+overflows_n = 0;
 
 
   
